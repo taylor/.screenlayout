324bc95665dee7077dfedeffb1f7cb01b91b7b46
Taylor Carpenter <taylor@codecafe.com>
2014-02-09 22:11:31 -0600

    change layout

--- appledisplay-and-laptop.sh
+++ appledisplay-and-laptop.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output DP3 --off --output DP2 --off --output DP1 --mode 2560x1440 --pos 1366x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x672 --rotate normal --output VGA1 --off
+#xrandr --output DP3 --off --output DP2 --off --output DP1 --mode 2560x1440 --pos 1366x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x672 --rotate normal --output VGA1 --off
+#xrandr --output DP3 --off --output DP2 --auto --output DP1 --auto --pos 1366x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x672 --rotate normal --output VGA1 --off
+#xrandr --output DP3 --off --output DP2 --mode 2560x1440 --pos 1366x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x672 --rotate normal --output VGA1 --off
+xrandr --output DP3 --off --output DP2 --mode 2560x1440 --pos 1366x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x672 --rotate normal --output VGA1 --off
+#xrandr --output DP3 --off --output DP2 --mode 2560x1440 --pos 1366x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1194x672 --rotate normal --output VGA1 --off
+
+disper -e -t left
